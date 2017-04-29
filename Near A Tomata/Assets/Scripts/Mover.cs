d7ee894f340a2240204a8e420f1160c10ce05780
WesLuong <wes.luong@gmail.com>
2017-04-29 16:52:04 -0700

    a

---
+++ Near A Tomata/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+    
+    private void Start() //code executed on first frame object instantiated
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+    }
+}
